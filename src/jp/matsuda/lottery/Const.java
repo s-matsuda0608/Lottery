01ed646919667993907a428f71a3e8736b72a830
s-matsuda0608 <shohei.matsuda@excite-software.net>
2015-09-29 15:11:45 +0900

    Const.java作成
    作成した。
    修正する可能性大

--- src/jp/matsuda/lottery/Const.java
+++ src/jp/matsuda/lottery/Const.java
@@ -2,4 +2,13 @@ package jp.matsuda.lottery;
 
 public class Const {
 
+	public static final String LOTTERY_LIST = "lotteryList";
+	public static final String LOTTERY_QUANTITY = "lotteryQuantity";
+	public static final CharSequence HAS_BLANK_DATA_MSG = "未入力欄があります";
+	public static final int REQUEST_CODE = 0;
+	public static final String OK = "OK";
+	public static final String CANCEL = "キャンセル";
+	protected static final CharSequence MORE_INPUT_MSG = "抽選対象を増やしてください";
+	public static final String WINNERS_LIST = "winnersListStr";
+
 }
