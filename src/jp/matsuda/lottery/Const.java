e9eb981a28bbc03a64e58675cad49409e93ac92a
s-matsuda0608 <shohei.matsuda@excite-software.net>
2015-10-01 11:00:54 +0900

    JavaDoc,コメント追加

--- src/jp/matsuda/lottery/Const.java
+++ src/jp/matsuda/lottery/Const.java
@@ -1,14 +1,36 @@
 package jp.matsuda.lottery;
 
+/**
+ * @author excite_2
+ * パッケージjp.matsuda.lotteryで用いるリテラルの定義のみしているConstanceクラス
+ * privateなコンストラクタを定義することによりインスタンスの生成をさせない。
+ */
 public class Const {
+	
+	//インスタンスの生成をさせないようにする
+	protected Const(){}
 
-	public static final String LOTTERY_LIST = "lotteryList";
-	public static final String LOTTERY_QUANTITY = "lotteryQuantity";
-	public static final CharSequence HAS_BLANK_DATA_MSG = "未入力欄があります";
-	public static final int REQUEST_CODE = 0;
-	public static final String OK = "OK";
-	public static final String CANCEL = "キャンセル";
+	
+	/** 抽選項目のリストをActivity間でやり取りするときのキー */
+	protected static final String LOTTERY_LIST = "lotteryList";
+	
+	/** 抽選数をActivity間でやり取りするときのキー */
+	protected static final String LOTTERY_QUANTITY = "lotteryQuantity";
+	
+	/** 当選した項目のリストをActivity間でやり取りするときのキー */
+	protected static final String WINNERS_LIST = "winnersListStr";
+	
+	/** 入力チェックをした結果、入力されていない項目があった時に表示するメッセージ */
+	protected static final CharSequence HAS_BLANK_DATA_MSG = "未入力欄があります";
+	
+	/** 抽選対象の項目が少ない時に表示するメッセージ */
 	protected static final CharSequence MORE_INPUT_MSG = "抽選対象を増やしてください";
-	public static final String WINNERS_LIST = "winnersListStr";
+	
+	/** ダイアログのOKボタン */
+	protected static final String OK = "OK";
+	
+	/** ダイアログのキャンセルボタン */
+	protected static final String CANCEL = "キャンセル";
+	
 
 }
--- src/jp/matsuda/lottery/LotteryConfirmActivity.java
+++ src/jp/matsuda/lottery/LotteryConfirmActivity.java
@@ -15,13 +15,29 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/**
+ * @author excite_2
+ * 抽選入力確認画面のActivity
+ * 入力情報を画面に表示する。
+ */
 public class LotteryConfirmActivity extends AppCompatActivity{
 
+	/** 入力された抽選対象項目を表示するLinearLayout */
 	private LinearLayout lotteryItemView;
+	
+	/** 入力された抽選数を表示するTextView */
 	private TextView lotteryQuantityView;
+	
+	/** OKボタン */
 	private Button OKButton;
+	
+	/** 戻るボタン */
 	private Button backButton;
+	
+	/** 入力された抽選項目のリスト */
 	private ArrayList<String> lotteryList = new ArrayList<String>();
+	
+	/** 入力された抽選数 */
 	private String lotteryQuantity;
 
 	@Override
@@ -29,6 +45,7 @@ public class LotteryConfirmActivity extends AppCompatActivity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lottery_confirm);
 
+		//ビュー取得
 		OKButton = (Button) findViewById(R.id.OKButton);
 		backButton = (Button) findViewById(R.id.backButton);
 		lotteryItemView = (LinearLayout) findViewById(R.id.lotteryItemView); 
@@ -36,6 +53,7 @@ public class LotteryConfirmActivity extends AppCompatActivity{
 
 		Intent intent = getIntent();
 
+		//入力データ取得
 		lotteryList = intent.getStringArrayListExtra(Const.LOTTERY_LIST);
 		lotteryQuantity = intent.getStringExtra(Const.LOTTERY_QUANTITY);
 
@@ -45,6 +63,9 @@ public class LotteryConfirmActivity extends AppCompatActivity{
 	}
 
 
+	/**
+	 * OKボタン、戻るボタンのリスナ登録
+	 */
 	private void setListener() {
 		
 		OKButton.setOnClickListener(new OnClickListener(){		
@@ -63,6 +84,9 @@ public class LotteryConfirmActivity extends AppCompatActivity{
 		
 	}
 	
+	/**
+	 * OKボタンが押されたときの処理
+	 */
 	private void onClickOKButton() {
 		Intent intent = new Intent(getApplication(), NowLotteryActivity.class);
 
@@ -73,22 +97,35 @@ public class LotteryConfirmActivity extends AppCompatActivity{
 		LotteryConfirmActivity.this.finish();
 	}
 
+	/**
+	 * 戻るボタンが押されたときの処理
+	 */
 	private void onClickbackButton() {
 		finish();
 	}
 
+	/**
+	 * 入力データを確認するビューの作成
+	 */
 	private void createConfirmView() {
 
 		lotteryItemView.removeAllViews();
 		
+		//抽選項目のリストをすべてビューに追加
 		for(int i = 0; i < lotteryList.size(); i++){	
+			
 			TextView tv = new TextView(this);
+			
+			//TextViewのスタイル設定
 			tv.setTextColor(Color.BLACK);
 			tv.setTextSize(20);
 			tv.setGravity(Gravity.CENTER_HORIZONTAL);
 			tv.setText(lotteryList.get(i));
+			
 			lotteryItemView.addView(tv);
 		}	
+		
+		//抽選数をビューに追加
 		lotteryQuantityView.setText(lotteryQuantity);	
 
 
